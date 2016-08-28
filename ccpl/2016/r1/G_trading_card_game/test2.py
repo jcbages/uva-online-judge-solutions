53f2ccf25c61beef462a2b31e3ad18fa7f661e83
Juan Camilo Bages <jcbages@outlook.com>
2016-08-28 14:59:16 -0500

    removed old folders

--- ccpl/2016/r1/G_trading_card_game/test2.py
+++
@@ -1,7 +0,0 @@
-from itertools import product
-N = 3; m = 5; k = 4
-nums = [x for x in range(1, N+1)]
-allperms = [x for x in product(nums, repeat=m)]
-valid = [x for x in allperms if len(set(x)) == k]
-print len(valid)
-print len(allperms)
