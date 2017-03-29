c8761b928ec9bd6ee75ebf3961bbec0bc1dc1107
Juan Camilo Bages <jcbages@outlook.com>
2017-03-28 22:26:59 -0500

    This repo now contains solutions only for the UVa Online Judge

--- poj/1000 - A+B Problem.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Main {
-	public static void main(String... args) {
-		Scanner sc = new Scanner(System.in);
-		int ans = sc.nextInt() + sc.nextInt();
-		System.out.println(ans);
-	}
-}
