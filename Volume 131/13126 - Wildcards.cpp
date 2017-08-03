32957abb3fb070daa5b9929ec77ca80cb6c6319f
Juan Camilo Bages <jcbages@outlook.com>
2017-08-03 17:43:09 -0500

    New solution to 13126 with hashing

--- Volume 131/13126 - Wildcards.cpp
+++ Volume 131/13126 - Wildcards.cpp
@@ -1,158 +1,46 @@
-#include <array>
 #include <iostream>
-#include <functional>
-#include <queue>
-#include <set>
-#include <string>
-#include <utility>
+#include <unordered_set>
 #include <vector>
 using namespace std;
-
-const int LETT = 26, MAX = 100005;
-
-int ans;
-string t, w;
-vector<int> dist;
-vector<string> pieces;
-int g[MAX][LETT], f[MAX];
-set<int> output[MAX];
-priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
-
-void preprocess_pattern() {
-	dist.clear();
-	pieces.clear();
-	string actual;
-	int count = 0;
-	for (int i = 0; i < w.size(); i++) {
-		if (w[i] == '?') {
-			count++;
-			if (actual.size() > 0) {
-				pieces.push_back(actual);
-				actual.clear();
-			}
-		} else {
-			if (i == 0 || count > 0) {
-				dist.push_back(count);
-				count = 0;
-			}
-			actual.push_back(w[i]);
-		}
-	}
-	if (actual.size() > 0) {
-		pieces.push_back(actual);
-		actual.clear();
-	}
-	dist.push_back(count);
-}
-
-void insert(int piece, string& s, int& newstate) {
-	int state = 0, j = 0;
-	while (j < s.size() && g[state][s[j]-'a'] != -1) {
-		state = g[state][s[j]-'a'];
-		j++;
-	}
-	for (; j < s.size(); j++) {
-		newstate++;
-		g[state][s[j]-'a'] = newstate;
-		state = newstate;
-	}
-	output[state].insert(piece);
-}
-
-void goto_function() {
-	for (int i = 0; i < MAX; i++) {
-		for (int j = 0; j < LETT; j++)
-			g[i][j] = -1;
-		output[i].clear();
-	}
-
-	int newstate = 0;
-	for (int i = 0; i < pieces.size(); i++) {
-		insert(i, pieces[i], newstate);
-	}
-	for (int i = 0; i < LETT; i++)
-		if (g[0][i] == -1)
-			g[0][i] = 0;
-}
-
-void failure_function() {
-	for (int i = 0; i < MAX; i++)
-		f[i] = -1;
-
-	int state;
-	queue<int> q;
-	for (int i = 0; i < LETT; i++) {
-		if (g[0][i] != 0) {
-			q.push(g[0][i]);
-			f[g[0][i]] = 0;
-		}
-	}
-	while (!q.empty()) {
-		int r = q.front(); q.pop();
-		for (int i = 0; i < LETT; i++) {
-			int s = g[r][i];
-			if (s != -1) {
-				q.push(s);
-				state = f[r];
-				while (g[state][i] == -1)
-					state = f[state];
-				f[s] = g[state][i];
-				for (set<int>::iterator it = output[f[s]].begin(); it != output[f[s]].end(); it++)
-					output[s].insert(*it);
-			}
-		}
-	}
-}
-
-void aho_corasick() {
-	// build the goto function and partial output function
-	goto_function();
-	// build the failure function and complete output function
-	failure_function();
-	// search for occurrences on the given text
-	int state = 0, n = dist.size();
-	for (int i = dist[0]; i < t.size() - dist[n-1]; i++) {
-		while (g[state][t[i]-'a'] == -1)
-			state = f[state];
-		state = g[state][t[i]-'a'];
-		// try to add new possible occs
-		if (output[state].find(0) != output[state].end()) { // possible occ
-			if (pieces.size() > 1)
-				pq.push(make_pair(i + pieces[1].size() + dist[1], 1));
-			else
-				ans++;
-		}
-		// try to get next values from pq
-		while (!pq.empty() && pq.top().first == i) {
-			int curr = pq.top().second; pq.pop();
-			if (output[state].find(curr) != output[state].end()) {
-				if (curr == pieces.size() - 1)
-					ans++;
-				else
-					pq.push(make_pair(i + pieces[curr+1].size() + dist[curr+1], curr+1));
-			}
-		}
-	}
-}
+#define b 123L
+#define MOD 1000000007L
+#define MAX 100005
+#define mod(a) (((a)%(MOD)+(MOD))%(MOD))
 
 int main() {
-	while (cin >> t && cin >> w) {
-		// special case #1: |w| > |t|
-		if (w.size() > t.size()) {
-			cout << 0 << endl;
-			continue;
+	iostream::sync_with_stdio(false); cin.tie(NULL);
+	int n, m, ans;
+	string t, w;
+	vector<int> wc;
+	long long h[MAX], r[MAX], sub[MAX], val, curr;
+	while (cin >> t >> w) {
+		wc.clear();
+		n = t.size(), m = w.size(), val = 0;
+		for (int i = 1, c; i <= m; i++) {
+			if (w[i-1] == '?') wc.push_back(i);
+			c = (w[i-1] == '?') ? 0 : w[i-1];
+			val = (val*b+c)%MOD;
 		}
-		// get pieces and distances
-		preprocess_pattern();
-		// special case #2: just wildcards (???)
-		if (pieces.size() == 0) {
-			cout << t.size() - w.size() + 1 << endl;
-			continue;
+
+		h[0] = 0, r[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			h[i] = (h[i-1]*b+t[i-1])%MOD;
+			r[i] = (r[i-1]*b)%MOD;
 		}
-		// get occurrences aho-corasick =)
+
+		for (int i = 0; i <= n-m; i++) {
+			sub[i] = 0;
+			for (int j = 0; j < wc.size(); j++) {
+				sub[i] = (sub[i]+t[wc[j]+i-1]*r[m-wc[j]])%MOD;
+			}
+		}
+
 		ans = 0;
-		pq = priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > >();
-		aho_corasick();
-		cout << ans << endl;
+		for (int i = m; i <= n; i++) {
+			curr = mod(h[i]-mod(h[i-m]*r[m]));
+			curr = mod(curr-sub[i-m]);
+			ans += (curr == val);
+		}
+		cout << ans << '\n';
 	}
 }
